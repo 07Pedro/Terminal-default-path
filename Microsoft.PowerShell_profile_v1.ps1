85e73777bbd31cffb0092f20531b9f222d8125eb
07Pedro <149793367+07Pedro@users.noreply.github.com>
2024-06-21 23:47:22 +0200

    Update Microsoft.PowerShell_profile_v1.ps1

--- Microsoft.PowerShell_profile_v1.ps1
+++ Microsoft.PowerShell_profile_v1.ps1
@@ -13,47 +13,25 @@ function Set-Directory {
 
     switch ($day) {
         'Monday' {
-            Set-Location 'C:\Users\e3ccep'
+            Set-Location ''
         }
         'Tuesday' {
-            Set-Location 'C:\Users\e3ccep'
+            Set-Location ''
         }
         'Wednesday' {
-            Set-Location 'C:\Users\e3ccep'
+            Set-Location ''
         }
         'Thursday' {
-            if ($hour -eq 8 -and $minute -eq 20) {
-                Set-Location 'C:\Path\For\Thursday\0820'
-            } elseif ($hour -ge 12 -and $hour -lt 18) {
-                Set-Location 'C:\Path\For\Thursday\Afternoon'
-            } elseif ($hour -ge 12 -and $hour -lt 18) {
-                Set-Location 'C:\Path\For\Thursday\Afternoon'
-            } elseif ($hour -ge 12 -and $hour -lt 18) {
-                Set-Location 'C:\Path\For\Thursday\Afternoon'
-            } elseif ($hour -ge 12 -and $hour -lt 18) {
-                Set-Location 'C:\Path\For\Thursday\Afternoon'
-            } else {
-                Set-Location 'C:\Users\e3ccep'
-            }
+            Set-Location ''
         }
         'Friday' {
-           if ($hour -eq 18 -and $minute -ge 0 -and $minute -lt 10) {
-                Set-Location 'C:\Path\For\Friday\Morning'
-            } elseif ($hour -ge 12 -and $hour -lt 18) {
-                Set-Location 'C:\Path\For\Friday\Afternoon'
-            } elseif ($hour -ge 12 -and $hour -lt 18) {
-                Set-Location 'C:\Path\For\Friday\Afternoon'
-            } elseif ($hour -ge 12 -and $hour -lt 18) {
-                Set-Location 'C:\Path\For\Friday\Afternoon'
-            } else {
-                Set-Location 'C:\Users\e3ccep'
-            }
+            Set-Location ''
         }
         'Saturday' {
-            Set-Location 'C:\Users\e3ccep'
+            Set-Location ''
         }
         'Sunday' {
-            Set-Location 'C:\Users\e3ccep'
+            Set-Location ''
         }
     }
 }
