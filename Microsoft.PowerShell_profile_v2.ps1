75793489bbb4a01618f8cf29949bb7e138e3ff49
07Pedro <149793367+07Pedro@users.noreply.github.com>
2024-06-21 23:31:11 +0200

    Update Microsoft.PowerShell_profile_v2.ps1

--- Microsoft.PowerShell_profile_v2.ps1
+++ Microsoft.PowerShell_profile_v2.ps1
@@ -11,28 +11,28 @@ function Set-Directory {
 
     switch ($day) {
         'Monday' {
-            if ($hour -eq 18 -and $minute -ge 30 -and $minute -lt 60) {
-                # Set-Location 'Your\Desired\Path\For\Monday\Evening'
-            } elseif ($hour -eq 12 -and $minute -ge 0 -and $minute -lt 60) {
+            if ($hour -eq 0 -and $minute -ge 0 -and $minute -lt 0) {
+                # Set-Location 'Your\Desired\Path'
+            } elseif ($hour -eq 0 -and $minute -ge 0 -and $minute -lt 0) {
                 # Set-Location 'Your\Desired\Path\For\Monday\Noon'
             } else {
-                # Set-Location 'Your\Desired\Path\For\Monday\Default'
+                # Set-Location 'Your\Desired\Path'
             }
         }
         'Tuesday' {
-            Set-Location 'C:\Users\e3ccep'
+            Set-Location ''
         }
         'Wednesday' {
-            Set-Location 'C:\Users\e3ccep'
+            Set-Location ''
         }
         'Thursday' {
-            Set-Location 'C:\Users\e3ccep'
+            Set-Location ''
         }
         'Friday' {
-            Set-Location 'C:\Users\e3ccep'
+            Set-Location ''
         }
         'Saturday' {
-            Set-Location 'C:\Users\e3ccep'
+            Set-Location ''
         }
         'Sunday' {
             Set-Location 'C:\Users\e3ccep'
